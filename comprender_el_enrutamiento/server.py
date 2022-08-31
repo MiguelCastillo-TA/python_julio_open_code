45b517260cd122130d5bab0825cd5aecab472c99
Miguel Castillo <mcastillo@codingdojo.cl>
2022-08-30 18:04:01 -0600

    Comprender el enrutamiento

---
+++ comprender_el_enrutamiento/server.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hola Mundo!"
+
+@app.route('/say/<name>')
+def saludo(name):
+    return f"Hola {name}!"
+
+@app.route('/repeat/<int:num>/<string:name>')
+def repeat(num, name):
+    # print(f"{name * 10}", end="\n")
+    # john<br>john<br>john<br>john<br>
+    # return f"{name * int(num)} <br>"
+    response = ""
+    for x in range(0, num):
+        # print(f"{name}", end="\n")
+        response = response + "<h1>" +name + "</h1>"
+    print(response)
+    return response
+
+# PATIO DE JUEGOS
+@app.route('/play')
+def play():
+    return render_template("play.html")
+
+
+# CATCH ALL
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return "¡Lo siento! No hay respuesta. Inténtalo otra vez."
+
+if __name__ == "__main__":
+    app.run(debug=True)
