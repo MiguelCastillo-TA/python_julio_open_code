442b2cddde4e91d6352636fbc25d54a62c6002c8
Miguel Castillo <mcastillo@codingdojo.cl>
2022-09-08 18:01:55 -0600

    user CRUD

--- usuarios_cr/server.py
+++ usuarios_cr/server.py
@@ -10,6 +10,7 @@ def all_users():
     print(all_users)
     return render_template('index.html', all_users=all_users)
 
+
 @app.route('/users/new', methods=['GET', 'POST'])
 def add_new_user():
     print(f"REQUEST TYPE ", request.method)
@@ -30,5 +31,46 @@ def add_new_user():
             flash('There was an error inserting a new user' ,'error')
             return redirect('/users/new')
 
+
+@app.route('/users/<id>')
+def view_user_details(id):
+    data = {
+        "id": id
+    }
+    user = User.get_user_by_id(data)
+    return render_template('userDetails.html', user=user)
+
+
+@app.route('/users/<id>/edit', methods=['GET', 'POST'])
+def edit_user_details(id):
+    if request.method == 'GET':
+        data = {
+            "id": id
+        }
+        user = User.get_user_by_id(data)
+        return render_template('editUserDetails.html', user=user)
+
+    elif request.method == 'POST':
+        print('REACHED EDIT POST')
+        data = {
+            "first_name": request.form['first_name'],
+            "last_name": request.form['last_name'],
+            "email": request.form['email'],
+            "id": id
+        }
+        user = User.edit_user_by_id(data)
+        return redirect('/users')
+        # print('we got ...')
+        # print(user)
+        # IF STATEMENT TO CHECK FOR ERROR
+ 
+@app.route('/users/<id>/delete')
+def delete_user_by_id(id):
+    data = {
+        "id": id
+    }
+    user = User.delete_user_by_id(data)
+    return redirect('/users')
+    
 if __name__ == "__main__":
     app.run(debug=True)
--- usuarios_cr/user.py
+++ usuarios_cr/user.py
@@ -29,5 +29,32 @@ class User:
         query = f"INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES (%(first_name)s, %(last_name)s, %(email)s, NOW(), NOW());"
         results = connectToMySQL('usuarios_cr').query_db(query, data)
         return results
+
+    @classmethod
+    def get_user_by_id(cls, data):
+        # SELECT * FROM users;
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        results = connectToMySQL('usuarios_cr').query_db(query, data)
+        user = results[0]
+        print(f"RESULTS: ", results[0])
+        return user
+
+    @classmethod
+    def edit_user_by_id(cls,data):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s, updated_at = NOW() WHERE (id = %(id)s);"
+        results = connectToMySQL('usuarios_cr').query_db(query, data)
+        print('RESUSLT DSFROM QUERY')
+        print(results)
+        # user = results[0]
+        #print(f"RESULTS: ", results[0])
+        return results
+
+    @classmethod
+    def delete_user_by_id(cls, data):
+        query = "DELETE FROM users WHERE (id = %(id)s);"
+        results = connectToMySQL('usuarios_cr').query_db(query, data)
+        print(results)
+        return results
+    
     # INSERT INTO `usuarios_cr`.`users` (`first_name`, `last_name`, `email`) VALUES ('NInja', 'ninja', 'ninja@email.com');
 
